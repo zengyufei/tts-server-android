bfa8a581297e2225a7737cc7a5b306e4f88ac89d
zengyufei <b17633601887@163.com>
2026-04-08 19:45:40 +0800

    fix12

--- lib-tts/src/main/java/com/github/jing332/tts/speech/plugin/engine/TtsPluginEngineV2.kt
+++ lib-tts/src/main/java/com/github/jing332/tts/speech/plugin/engine/TtsPluginEngineV2.kt
@@ -27,7 +27,8 @@ import java.io.InputStream
 import java.io.PipedInputStream
 import java.io.PipedOutputStream
 import java.lang.IllegalArgumentException
-import kotlin.math.roundToInt
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 open class TtsPluginEngineV2(val context: Context, var plugin: Plugin) {
     companion object {
@@ -214,7 +215,7 @@ open class TtsPluginEngineV2(val context: Context, var plugin: Plugin) {
         pitch: Float = 1f,
     ): InputStream {
         // Keep plugin rate in x-speed form (e.g. 1.1x), without extra scaling.
-        val r = (rate * 10f).roundToInt() / 10f
+        val r = BigDecimal(rate.toString()).setScale(1, RoundingMode.HALF_UP).toDouble()
         val v = (volume * 50f).toInt()
         val p = (pitch * 50f).toInt()
         val result = try {
